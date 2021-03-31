718a14119f381879c64ebf620e3be52fbaef81b6
yunwen <yunweneric@gmail.com>
2021-03-31 05:44:58 +0100

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>@yield('title')</title>
+    {{-- <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
+    <!-- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+        rel="stylesheet">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous">
+    </script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
+        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+
+    @section('sidebar')
+
+    @show
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
